847a081772f7e0a0d82d49b871b82c4e0533d436
Kaj Rumpff <rumpff.kaj@gmail.com>
2022-10-03 05:26:30 +0200

    Added visuals and physics to weapon pickup

--- Assets/Scripts/Game/GameObjects/WeaponPickup.cs
+++ Assets/Scripts/Game/GameObjects/WeaponPickup.cs
@@ -4,17 +4,27 @@ using UnityEngine;
 
 public class WeaponPickup : MonoBehaviour
 {
+    public bool AutoInitialize = false;
     public WeaponScriptableObject Weapon;
     [SerializeField] private Transform _modelParent;
 
+    public void Initialize()
+    {
+        Instantiate(Weapon.WeaponPrefab, _modelParent);
+    }
+
     void Start()
     {
-
+        if(AutoInitialize)
+            Initialize();
     }
 
     void Update()
     {
-
+        _modelParent.localPosition = new Vector3()
+        {
+            z = Mathf.Cos(Time.time * 2) * 0.25f
+        };
     }
 
     public void PickedUp()
